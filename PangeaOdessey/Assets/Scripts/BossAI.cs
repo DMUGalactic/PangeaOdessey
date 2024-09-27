567a351cf0e7f69dcce278f8f3d7b276eb2644ef
JinhoBa <127280695+JinhoBa@users.noreply.github.com>
2024-09-28 01:06:46 +0900

    노드 연결 작업
    보스 AI 스크립트 작성
    - bt 구조 작성
    - node가 실행할 일부 함수 작성

--- PangeaOdessey/Assets/Scripts/BossAI.cs
+++ PangeaOdessey/Assets/Scripts/BossAI.cs
@@ -1,18 +1,200 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BossAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Range")]
+    [SerializeField]
+    float _detectRange = 10f;
+    [SerializeField]
+    float _meleeAttackRange = 5f;
+
+    [Header("Movement")]
+    [SerializeField]
+    float _movementSpeed = 10f;
+
+    Vector2 _originPos = default;
+    BehaviorTreeRunner _BTRunner = null;
+    Transform _detectedPlayer = null;
+    Animator _animator = null;
+
+    const string _ATTACK_ANIM_STATE_NAME = "Attack";
+    const string _ATTACK_ANIM_TIRGGER_NAME = "attack";
+
+    void Awake()
     {
-        
+        _animator = GetComponent<Animator>();
+
+        _BTRunner = new BehaviorTreeRunner(SettingBT());
+
+        _originPos = transform.position;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        _BTRunner.Operate();
     }
+    INode SettingBT()
+    {
+        return new SelectorNode
+            (
+                new List<INode>()
+                {
+                    new SequenceNode
+                    (
+                        new List<INode>()
+                        {
+                            new ActionNode(CheckMeleeAttacking),
+                            new ActionNode(CheckEnemyWithinMeleeAttackRange),
+                            new ActionNode(DoMeleeAttack),
+                        }
+                    ),
+                    new SequenceNode
+                    (
+                        new List<INode>()
+                        {
+                            //new ActionNode(CheckRangedAttacking),
+                            //new ActionNode(CheckEnemyWithinRangedAttackRange),
+                            //new ActionNode(DoRangedAttack),
+                        }
+                    ),
+                    new SequenceNode
+                    (
+                        new List<INode>()
+                        {
+                            new ActionNode(CheckDetectEnemy),
+                            new ActionNode(MoveToDetectEnemy),
+                        }
+                    ),
+                    
+                }
+            );
+    }
+
+    // 시퀀스 상황에서 애니메이션 실행 후 다음 노드 실행
+    bool IsAnimationRunning(String stateName)
+    {
+        if(_animator != null)
+        {
+            if(_animator.GetCurrentAnimatorStateInfo(0).IsName(stateName))
+            {
+                var normalizedTime = _animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+
+                return normalizedTime != 0 && normalizedTime < 1f;
+            }
+        }
+
+        return false;
+    }
+
+    #region Melee Attack Node
+    INode.ENodeState CheckMeleeAttacking()
+    {
+        if (IsAnimationRunning(_ATTACK_ANIM_STATE_NAME))
+        {
+            return INode.ENodeState.ENS_Running;
+        }
+
+        return INode.ENodeState.ENS_Success;
+    }
+
+    INode.ENodeState CheckEnemyWithinMeleeAttackRange()
+    {
+        if (_detectedPlayer != null)
+        {
+            if (Vector2.SqrMagnitude(_detectedPlayer.position - transform.position) < (_meleeAttackRange * _meleeAttackRange))
+            {
+                return INode.ENodeState.ENS_Success;
+            }
+        }
+
+        return INode.ENodeState.ENS_Failure;
+    }
+
+     INode.ENodeState DoMeleeAttack()
+    {
+        if (_detectedPlayer != null)
+        {
+            _animator.SetTrigger(_ATTACK_ANIM_TIRGGER_NAME);
+            return INode.ENodeState.ENS_Success;
+        }
+
+        return INode.ENodeState.ENS_Failure;
+    }
+    #endregion
+
+    #region Melee Attack Node
+    INode.ENodeState CheckRangedAttacking()
+    {
+        if (IsAnimationRunning(_ATTACK_ANIM_STATE_NAME))
+        {
+            return INode.ENodeState.ENS_Running;
+        }
+
+        return INode.ENodeState.ENS_Success;
+    }
+
+    INode.ENodeState CheckEnemyWithinRangedAttackRange()
+    {
+        if (_detectedPlayer != null)
+        {
+            if (Vector2.SqrMagnitude(_detectedPlayer.position - transform.position) < (_meleeAttackRange * _meleeAttackRange))
+            {
+                return INode.ENodeState.ENS_Success;
+            }
+        }
+
+        return INode.ENodeState.ENS_Failure;
+    }
+
+     INode.ENodeState DoRangedAttack()
+    {
+        if (_detectedPlayer != null)
+        {
+            _animator.SetTrigger(_ATTACK_ANIM_TIRGGER_NAME);
+            return INode.ENodeState.ENS_Success;
+        }
+
+        return INode.ENodeState.ENS_Failure;
+    }
+    #endregion
+
+    #region Detect & Move Node
+    INode.ENodeState CheckDetectEnemy()
+    {
+        var overlapColliders = Physics.OverlapSphere(transform.position, _detectRange, LayerMask.GetMask("Player"));
+
+        if(overlapColliders != null && overlapColliders.Length > 0)
+        {
+            _detectedPlayer = overlapColliders[0].transform;
+
+            return INode.ENodeState.ENS_Success;
+        }
+
+        _detectedPlayer = null;
+
+        return INode.ENodeState.ENS_Failure;
+    }
+
+    INode.ENodeState MoveToDetectEnemy()
+    {
+        if (_detectedPlayer != null)
+        {
+            if (Vector3.SqrMagnitude(_detectedPlayer.position - transform.position) < (_meleeAttackRange * _meleeAttackRange))
+            {
+                return INode.ENodeState.ENS_Success;
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, _detectedPlayer.position, Time.deltaTime * _movementSpeed);
+
+            return INode.ENodeState.ENS_Running;
+        }
+
+        return INode.ENodeState.ENS_Failure;
+    }
+    #endregion
+
 }
