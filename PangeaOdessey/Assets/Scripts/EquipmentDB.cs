2b130295541ceadab5fc088c2484b78e72b1c940
Jihyun0126 <127286383+Jihyun0126@users.noreply.github.com>
2024-10-23 11:59:05 +0900

    DB빌드업

--- PangeaOdessey/Assets/Scripts/EquipmentDB.cs
+++ PangeaOdessey/Assets/Scripts/EquipmentDB.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[CreateAssetMenu(fileName="Equipment Info", menuName="Scriptable Object Asset/EquipmentInfo")]
 
 public class EquipmentDB : MonoBehaviour
 {
